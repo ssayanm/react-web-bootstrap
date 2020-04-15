2b3206e94a79ffb618e355fe133ca26d4fc53b8a
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-15 09:49:31 +0530

    created a basic website with react bootstrap, react-roter-dom

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+
+  a,
+  .navbar-brand .navbar-nav,
+  .nav-link {
+    color: red;
+
+    &.hover {
+      color: white;
+    }
+  }
+`;
+
+const NavigationBar = () => {
+  return (
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">Code Life</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/contact">Contact</Link>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
+  );
+};
+
+export default NavigationBar;
